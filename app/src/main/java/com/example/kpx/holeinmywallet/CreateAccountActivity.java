7b642a5352e43ad2a4dcc08eebe7b445d608cc13
Brandon <bshoc@bellsouth.net>
2017-10-14 12:33:19 -0400

    More firebase stuff. It's broken.

--- app/src/main/java/com/example/kpx/holeinmywallet/CreateAccountActivity.java
+++ app/src/main/java/com/example/kpx/holeinmywallet/CreateAccountActivity.java
@@ -1,11 +1,19 @@
 package com.example.kpx.holeinmywallet;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -14,10 +22,73 @@ public class CreateAccountActivity extends AppCompatActivity {
     TODO: Validate entered information
      */
 
+    private EditText emailField, passwordField, confirmPasswordField, accountIdField;
+    private FirebaseAuth authentication;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        //Fields
+        emailField = (EditText)findViewById(R.id.create_email_field);
+        passwordField = (EditText)findViewById(R.id.create_password_field);
+        confirmPasswordField = (EditText)findViewById(R.id.create_password_confirm_field);
+        accountIdField = (EditText)findViewById(R.id.create_account_id_field);
+
+        authentication = FirebaseAuth.getInstance();
+        authentication.signOut();
+    }
+
+    public void createAccount(View view) {
+        if (verifyFields()) {
+            Log.d("Login", "Fields verified");
+            authentication.createUserWithEmailAndPassword(emailField.toString(), passwordField.toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("Login", "Success");
+                                setResult(1);
+                            } else {
+                                setResult(0);
+                                Log.d("Login", "Failed");
+                            }
+                        }
+                    });
+            finish();
+        }
+    }
+
+    private boolean verifyFields() {
+        boolean valid = true;
+
+        String email = emailField.getText().toString();
+        if (!email.contains("@") || email.equals("")) {
+            valid = false;
+            emailField.setError("Please enter valid email address");
+        }
+
+        String password = passwordField.getText().toString();
+        String confirmPassword = passwordField.getText().toString();
+        if (password.length() < 8) {
+            valid = false;
+            passwordField.setError("Must be 8 characters or longer");
+        }
+        if (!password.equals(confirmPassword)) {
+            valid = false;
+            confirmPasswordField.setError("Passwords must match");
+        }
+
+        String accountId = accountIdField.getText().toString();
+        if (!TextUtils.isDigitsOnly(accountId)) {
+            valid = false;
+            accountIdField.setError("ID must consist of numbers only");
+        }
+        if (accountId.length() < -1) {
+            valid = false;
+            accountIdField.setError("ID must be 24 digits long");
+        }
+        return valid;
     }
 
 }
--- app/src/main/java/com/example/kpx/holeinmywallet/LoginActivity.java
+++ app/src/main/java/com/example/kpx/holeinmywallet/LoginActivity.java
@@ -1,22 +1,72 @@
 package com.example.kpx.holeinmywallet;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    static final int REQUEST_CODE = 1;
+
+    EditText emailField, passwordField;
+
+    FirebaseAuth authentication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        emailField = (EditText)findViewById(R.id.login_email_field);
+        passwordField = (EditText)findViewById(R.id.login_password_field);
+
+        authentication = FirebaseAuth.getInstance();
     }
 
     public void launchCreateAccount(View view) {
         Intent intent = new Intent(this, CreateAccountActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    public void signIn(View view) {
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+        authentication.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            launchMainActivity();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Could not sign in", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void launchMainActivity() {
+        Toast.makeText(getApplicationContext(), "Successfully signed in user: " + authentication.getCurrentUser().getUid(), Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == 1) {
+                //Success
+                Toast.makeText(getApplicationContext(), "Successfully created account", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Could not create account", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
