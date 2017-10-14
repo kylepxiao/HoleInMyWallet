7b3965665ada5704d42a9a8782a62c8cfe6c1335
Brandon <bshoc@bellsouth.net>
2017-10-14 02:24:33 -0400

    Added account creation activity and layout

---
+++ app/src/main/java/com/example/kpx/holeinmywallet/CreateAccountActivity.java
@@ -0,0 +1,23 @@
+package com.example.kpx.holeinmywallet;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    /*
+    TODO: Setup firebase authentication and database
+    TODO: Validate entered information
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+
+}
