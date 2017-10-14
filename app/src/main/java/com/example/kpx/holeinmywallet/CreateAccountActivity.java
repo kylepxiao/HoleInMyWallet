10275af0afd5656cd02bde3d7362812ed22ef264
Kyle Xiao <kylepxiao@gmail.com>
2017-10-14 05:47:30 -0400

    Added get request functionality for azure api

--- app/src/main/java/com/example/kpx/holeinmywallet/CreateAccountActivity.java
+++ app/src/main/java/com/example/kpx/holeinmywallet/CreateAccountActivity.java
@@ -5,7 +5,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -18,6 +31,45 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        //Logs a user recommendation
+        addUserRecommendationToQueue();
     }
 
+    private void addUserRecommendationToQueue(){
+        RequestQueue mRequestQueue = MySingleton.getInstance(this.getApplicationContext()).
+                getRequestQueue();
+
+        // Start the queue
+        mRequestQueue.start();
+
+        String url = "https://westus.api.cognitive.microsoft.com/recommendations/v4.0/models/864912de-b987-4f45-975b-4eadf4d35ae9/recommend/user?userId=85526&numberOfResults=1";
+
+        // Request a string response from the provided URL.
+        HashMap<String, String> parameters = new HashMap<String, String>();
+        parameters.put("includeMetaData", "false");
+        parameters.put("buildId", "1660942");
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, new JSONObject(parameters), new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("asdf", response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error", "Error with JSON Volley response");
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                //headers.put("Content-Type", "application/json");
+                headers.put("Ocp-Apim-Subscription-Key", "586a2994e7cb4523beb170e60f0954c3");
+                return headers;
+            }
+        };
+        // Add the request to the RequestQueue.
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+    }
 }
